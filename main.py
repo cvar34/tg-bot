9b1e3084081cb4c232f62b53ec103b312320bca2
cvar34 <daniilporoh2@gmail.com>
2026-03-17 18:22:52 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import telebot
+bot = telebot.TeleBot("")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
